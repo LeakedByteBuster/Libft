61278486fb62fd33eadb49764d65fce33a700fb2
Mushigarou <procompte97@gmail.com>
2022-10-23 21:52:15 +0100

    libft

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mfouadi <mfouadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 20:43:44 by mfouadi           #+#    #+#             */
-/*   Updated: 2022/10/23 21:03:22 by mfouadi          ###   ########.fr       */
+/*   Updated: 2022/10/23 21:47:26 by mfouadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t len_s1;
 	size_t len_s2;
 
+	if (!s1)
+		return (ft_strdup(s2));
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
-	
 	newStr = (char *)malloc(len_s1 + len_s2 + NULL_CHAR);
-	if (!newStr)
+	if (newStr == NULL)
 		return (newStr);
-	while (s1++)
-		*newStr++ = *(char *)s1++;
-	newStr += len_s1;
-	while (s2++)
-		*newStr++ = *(char *)s2++;
-	*newStr = '\0';
+	len_s1 = 0;
+	len_s2 = 0;
+	while (s1[len_s1])
+		newStr[len_s2++] = s1[len_s1++];
+	len_s1 = 0;
+	while (s2[len_s1])
+		newStr[len_s2++] = s2[len_s1++];
+	newStr[len_s2] = '\0';
 	return (newStr);
 }
