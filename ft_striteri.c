7e5d5edca919405d0c77fcdc1fddae874e015fb7
Mushigarou <procompte97@gmail.com>
2022-11-02 11:35:44 +0100

    Applies func f to each char in s

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,13 +6,21 @@
 /*   By: mfouadi <mfouadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 10:07:44 by mfouadi           #+#    #+#             */
-/*   Updated: 2022/11/02 10:16:12 by mfouadi          ###   ########.fr       */
+/*   Updated: 2022/11/02 11:34:37 by mfouadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// void ft_striteri(char *s, void (*f)(unsigned int, char*))
-// {
-    
-// }
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int i;
+
+	i = 0;
+	while (*s)
+	{
+		f(i, s);
+		i++;
+		s++;
+	}
+}
