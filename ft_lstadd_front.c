9cb084a4dac9e2f82a437ba28cbc1bfa4af6be44
Mushigarou <procompte97@gmail.com>
2022-11-05 01:24:36 +0100

    Adds the node to the beginning of the list

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,21 +6,32 @@
 /*   By: mfouadi <mfouadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 07:36:38 by mfouadi           #+#    #+#             */
-/*   Updated: 2022/11/04 22:31:20 by mfouadi          ###   ########.fr       */
+/*   Updated: 2022/11/05 01:23:38 by mfouadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Adds the node ’new’ at the beginning of the list.
 #include "libft.h"
 
-// void ft_lstadd_front(t_list **lst, t_list *new)
+void ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst && *lst && new)
+		new->next = *lst;
+}
+// #include<stdio.h>
+// int main()
 // {
-// 	t_list *p;
+// 	t_list *t1 = ft_lstnew("Hello ");
+// 	t_list *t2 =ft_lstnew("World");
+// 	t_list *t3 = ft_lstnew("!\n");
 	
-// 	p = new;
-// 	if (new == NULL)
-// 		return (p->content = NULL, exit());
-// 	lst = &new;
-// 	p->next = *lst;
-// 	p->content = new;
+// 	t_list *tmp;
+// 	ft_lstadd_front(&t3, t2);
+// 	ft_lstadd_front(&t2, t1);
+// 	tmp = t1;
+// 	while(tmp)
+// 	{
+// 		printf("%s", (char *)tmp->content);
+// 		tmp=tmp->next;
+// 	}
 // }
