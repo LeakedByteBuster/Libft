51e9a8d3a095596ae2051c2e4b23f56be731d5dc
Mushigarou <procompte97@gmail.com>
2022-11-06 02:39:53 +0100

    handling overlap of the string

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mfouadi <mfouadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 19:47:20 by mfouadi           #+#    #+#             */
-/*   Updated: 2022/10/23 01:25:25 by mfouadi          ###   ########.fr       */
+/*   Updated: 2022/11/06 02:38:38 by mfouadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,18 @@ void    *ft_memmove(void *dst, const void *src, size_t len)
     size_t tmp;
 
     tmp = len;
-    if (len == 0 || (src == NULL && dst == NULL))
-        return (dst);
-    while (len-- > 0)
-        *(char *)dst++ = *(char *)src++;
-    return (dst - tmp);
+    char *cast_dst = (char *)dst;
+    char *cast_src = (char *)src;
+    if (dst > src)
+        while (len-- > 0)
+            cast_dst[len] = cast_src[len];
+    else
+        ft_memcpy(dst, src, len);
+    return (dst);
+    
+    // if (len == 0 || (src == NULL && dst == NULL))
+    //     return (dst);
+    // while (len-- > 0)
+    //     *(char *)dst++ = *(char *)src++;
+    // return (dst - tmp);
 }
