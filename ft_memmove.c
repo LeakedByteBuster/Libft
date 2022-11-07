984335f11b1fffa0afd806a28f650f1495d3131f
Mushigarou <procompte97@gmail.com>
2022-11-07 02:49:09 +0100

    handling overlap

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,31 +6,37 @@
 /*   By: mfouadi <mfouadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 19:47:20 by mfouadi           #+#    #+#             */
-/*   Updated: 2022/11/06 02:38:38 by mfouadi          ###   ########.fr       */
+/*   Updated: 2022/11/06 03:13:42 by mfouadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //copy byte string
 //The two strings may overlap
 #include "libft.h"
+#include <stdio.h>
 
 void    *ft_memmove(void *dst, const void *src, size_t len)
 {
-    size_t tmp;
+	size_t tmp;
 
-    tmp = len;
-    char *cast_dst = (char *)dst;
-    char *cast_src = (char *)src;
-    if (dst > src)
-        while (len-- > 0)
-            cast_dst[len] = cast_src[len];
-    else
-        ft_memcpy(dst, src, len);
-    return (dst);
-    
-    // if (len == 0 || (src == NULL && dst == NULL))
-    //     return (dst);
-    // while (len-- > 0)
-    //     *(char *)dst++ = *(char *)src++;
-    // return (dst - tmp);
+	tmp = len;
+	char *cast_dst = (char *)dst;
+	char *cast_src = (char *)src;
+	if (dst > src)
+		while (len-- > 0)
+			cast_dst[len] = cast_src[len];
+	else
+		ft_memcpy(dst, src, len);
+	return (dst);
+
+	// if (len == 0 || (src == NULL && dst == NULL))
+	//     return (dst);
+	// while (len-- > 0)
+	//     *(char *)dst++ = *(char *)src++;
+	// return (dst - tmp);
 }
+// int main()
+// {
+// 	char *p = "0123456789"; //0x0 0x2
+// 	printf("%s", ft_memmove((&p + 3), p, 30));
+// }
