c38aa56387b445eb4c01cc3bd3b5d7ae52b421fb
Mushigarou <procompte97@gmail.com>
2022-11-11 06:51:24 +0100

    fixing the issue of allocating the good size

--- ft_substr.c
+++ ft_substr.c
@@ -10,28 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// Allocates (with malloc(3)) and returns a substring from the string ’s’.
-// The substring begins at index ’start’ and is of maximum size ’len’.
 #include "libft.h"
+
+// Counting the len of the new str
+
+static	size_t	ft_count_lenstr(const char *s, unsigned int start, size_t len)
+{
+	size_t	i;
+
+	i = start;
+	while (s[i] != '\0' && len > 0)
+	{
+		i++;
+		len--;
+	}
+	return (i);
+}
+
+/*Returns a substr from the given string, and allocates with malloc(3) 
+based on : where to start? and where to end?*/
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*ptr;
-	size_t			len_s;
 	unsigned int	i;
 
-	if (s == NULL || len == 0)
-		return (NULL);
-	len_s = ft_strlen(s);
-	if (len_s >= len + start)
-		ptr = (char *)malloc(len + NULL_CHAR);
-	else
+	i = ft_strlen(s);
+	if (s == NULL || len <= 0 || start > i)
 		return (ft_strdup(""));
+	if (i >= (start + len))
+		i = len;
+	else
+		i = ft_count_lenstr(s, start, len);
+	ptr = (char *)malloc(i + NULL_CHAR);
 	if (!ptr)
 		return (ptr);
 	s += start;
 	i = 0;
-	while (len-- && s)
+	while (len-- > 0 && *s != '\0')
 	{
 		*ptr++ = *s++;
 		i++;
@@ -39,3 +55,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	*ptr = '\0';
 	return (ptr - i);
 }
+// #include <stdio.h>
+// int main()
+// {
+// 	printf("%s\n", ft_substr("hola", 2, 210315));
+// 	printf("%zu", ft_strlen(ft_substr("hola", 0, 210315)));
+
+// }
