eb9e1d020ad087ee220939665706eb8deadbd687
Mushigarou <procompte97@gmail.com>
2022-11-14 01:44:15 +0100

    fix: 2 leaks and 1 sigfault to be fixed

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -10,52 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-	/* Iterates the list ’lst’ and applies the function
-	’f’ on the content of each node. Creates a new
-	list resulting of the successive applications of
-	the function ’f’. The ’del’ function is used to
-	delete the content of a node if needed. 
-	Returns : A new list, NULL if Alloc fails */
+/* Iterates the list ’lst’ and applies the function
+’f’ on the content of each node. Creates a new
+list resulting of the successive applications of
+the function ’f’. The ’del’ function is used to
+delete the content of a node if needed. 
+Returns : A new list, NULL if Alloc fails */
 
 #include "libft.h"
 
-static void	free_mem(t_list *p)
-{
-	t_list	*tmp;
+// static void	free_mem(t_list *p)
+// {
+// 	t_list	*tmp;
 
-	tmp = p;
-	while (tmp != NULL)
-	{
-		tmp = tmp->next;
-		free(p);
-		p = tmp;
-	}
-}
+// 	tmp = p;
+// 	while (tmp != NULL)
+// 	{
+// 		tmp = tmp->next;
+// 		free(p);
+// 		p = tmp;
+// 	}
+// }
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*tmp_head;
-	t_list	*tmp_lst;
 	t_list	*head;
 
-	// tmp_head = NULL;
 	if (lst == NULL)
 		return (NULL);
-	tmp_head = ft_lstnew(f(lst->content));
+	tmp_head = ft_lstnew((f(lst->content)));
 	if (tmp_head == NULL)
 		return (NULL);
+	lst = lst->next;
+	// tmp_head = tmp_head->next;
 	head = tmp_head;
-	tmp_lst = lst;
-	while (tmp_lst != NULL)
+	while (lst != NULL)
 	{
-		tmp_head->next = ft_lstnew(f(lst->content));
-		if (tmp_head == NULL)
-			return (free_mem(head), NULL);
-		tmp_lst = tmp_lst->next;
-		tmp_head = tmp_head->next;
+		tmp_head->next = ft_lstnew((f(lst->content)));
+		if (!tmp_head)
+			return  (ft_lstclear(&head, del), NULL);
+	 	lst = lst->next;
+	 	tmp_head = tmp_head->next;
 	}
-	if (del != NULL)
-		del(head->content);
 	return (head);
 }
 
