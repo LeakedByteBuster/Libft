e6b936e5916559d4ce6b968e87d484e09573fdcf
Mushigarou <procompte97@gmail.com>
2022-11-14 16:34:51 +0100

    fix: 2 leaks fixed, 1 seg left

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -16,7 +16,9 @@ list resulting of the successive applications of
 the function ’f’. The ’del’ function is used to
 delete the content of a node if needed. 
 Returns : A new list, NULL if Alloc fails */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "libft.h"
 
 // static void	free_mem(t_list *p)
@@ -32,6 +34,24 @@ Returns : A new list, NULL if Alloc fails */
 // 	}
 // }
 
+// void *map_length(void *s)
+// {
+// 	char *str = malloc(10);
+// 	if (str != NULL)
+// 		sprintf(str, "__%lX", strlen((char *)s));
+// 	return str;
+// }
+
+// void delete(void *content)
+// {
+// 	char *str = (char *)content;
+// 	if (str == NULL)
+// 		return;
+// 	if (str[0] != '_' || str[1] != '_')
+// 		printf("error\n");
+// 	free(content);
+// }
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*tmp_head;
@@ -39,15 +59,21 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if (lst == NULL)
 		return (NULL);
-	tmp_head = ft_lstnew((f(lst->content)));
-	if (tmp_head == NULL)
+	char *str = f(lst->content);
+	if (str == NULL)
 		return (NULL);
+	tmp_head = ft_lstnew(str);
+	if (tmp_head == NULL)
+		return (del(str), NULL);
 	lst = lst->next;
 	// tmp_head = tmp_head->next;
 	head = tmp_head;
 	while (lst != NULL)
 	{
-		tmp_head->next = ft_lstnew((f(lst->content)));
+		str = f(lst->content);
+		if (str == NULL)
+			return (ft_lstclear(&head, del), NULL);
+		tmp_head->next = ft_lstnew(str);
 		if (!tmp_head)
 			return  (ft_lstclear(&head, del), NULL);
 	 	lst = lst->next;
@@ -56,17 +82,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	return (head);
 }
 
+// int main()
+// {
+// 	t_list *head = ft_lstnew(ft_strdup("haitam"));
+// 	t_list *res = ft_lstmap(head, map_length, delete);
+// 	printf("str == %s\n", res->content);
+// }
+
 // void    *f(void *p)
 // {
-//     t_list *tmp;
-
-//     tmp = (t_list *)p;
-//     while (tmp != NULL)
-//     {
-//         tmp->content = "a";
-//         tmp = tmp->next;
-//     }
-//     return p;
+// 	p = ft_strdup("a");
+// 	return (p);
 // }
 
 // #include <stdio.h>
@@ -74,16 +100,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 //     t_list *v0;
 //     t_list *v1;
 //     t_list *v2;
+//     t_list *v3;
 //     t_list *tmp;	
 //     v0 = ft_lstnew("h");
 //     v1 = ft_lstnew("g");
 //     v2 = ft_lstnew("g");
+//     v3 = ft_lstnew("s");
 
 //     ft_lstadd_front(&v1, v0);
 //     ft_lstadd_back(&v1, v2);
+//     ft_lstadd_back(&v2, v3);
 
 //     tmp = v0;
-//     ft_lstmap(tmp, f(tmp), NULL);
+//     ft_lstmap(tmp, f, free);
 //     while (tmp != NULL)
 //     {
 //         printf("%s\n", tmp->content);
